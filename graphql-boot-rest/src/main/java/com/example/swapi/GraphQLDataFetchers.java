d70080c03a7b9388eba310e730dcd5e618e6e125
GaryChengld <chengld@yahoo.com>
2019-09-29 22:07:19 -0400

    about query

--- graphql-boot-rest/src/main/java/com/example/swapi/GraphQLDataFetchers.java
+++ graphql-boot-rest/src/main/java/com/example/swapi/GraphQLDataFetchers.java
@@ -1,7 +1,24 @@
 package com.example.swapi;
 
+import graphql.schema.DataFetcher;
+import org.springframework.stereotype.Component;
+
 /**
  * @author Gary Cheng
  */
+@Component
 public class GraphQLDataFetchers {
+    private String aboutMessage = "Star Wars GraphQL Example v1.0";
+
+    public DataFetcher getAboutMessage() {
+        return dataFetchingEnvironment -> aboutMessage;
+    }
+
+    public DataFetcher setAboutMessage() {
+        return dataFetchingEnvironment -> {
+            String message = dataFetchingEnvironment.getArgument("message");
+            this.aboutMessage = message;
+            return message;
+        };
+    }
 }
--- graphql-boot-rest/src/main/java/com/example/swapi/GraphQLTypeResolver.java
+++ graphql-boot-rest/src/main/java/com/example/swapi/GraphQLTypeResolver.java
@@ -1,7 +1,21 @@
-package com.example.swapi.data;
+package com.example.swapi;
+
+import com.example.swapi.data.Human;
+import com.example.swapi.data.MovieCharacter;
+import graphql.schema.GraphQLObjectType;
+import graphql.schema.TypeResolver;
 
 /**
  * @author Gary Cheng
  */
-public class GraphqlTypeResolver {
+public class GraphQLTypeResolver {
+
+    TypeResolver characterTypeResolver = environment -> {
+        MovieCharacter character = environment.getObject();
+        if (character instanceof Human) {
+            return (GraphQLObjectType) environment.getSchema().getType("Human");
+        } else {
+            return (GraphQLObjectType) environment.getSchema().getType("Droid");
+        }
+    };
 }
