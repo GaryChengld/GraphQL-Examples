7c858adc48dbfd6793b94013d8071fc8ddea037e
Gary Cheng <chengld@yahoo.com>
2019-10-01 14:49:29 -0400

    Update GraphQLProvider.java

--- graphql-java-datafetcher/src/main/java/com/example/swapi/GraphQLProvider.java
+++ graphql-java-datafetcher/src/main/java/com/example/swapi/GraphQLProvider.java
@@ -12,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -27,16 +28,21 @@ import static graphql.schema.idl.TypeRuntimeWiring.newTypeWiring;
 @Configuration
 @Slf4j
 public class GraphQLProvider {
-    @Autowired
+    private static final String SCHEMA_RESOURCE = "swapi.graphql";
     private GraphQLDataFetchers graphQLDataFetchers;
-    @Autowired
     private GraphQLTypeResolver graphQLTypeResolver;
     private GraphQL graphQL;
+
+    @Autowired
+    public GraphQLProvider(GraphQLDataFetchers graphQLDataFetchers, GraphQLTypeResolver graphQLTypeResolver) {
+        this.graphQLDataFetchers = graphQLDataFetchers;
+        this.graphQLTypeResolver = graphQLTypeResolver;
+    }
 
     @PostConstruct
     public void init() throws IOException {
         log.info("init GraphQLProvider");
-        URL url = Resources.getResource("swapi.graphql");
+        URL url = Resources.getResource(SCHEMA_RESOURCE);
         String sdl = Resources.toString(url, Charsets.UTF_8);
         GraphQLSchema graphQLSchema = buildSchema(sdl);
         this.graphQL = GraphQL.newGraphQL(graphQLSchema).build();
@@ -52,28 +58,28 @@ public class GraphQLProvider {
     private RuntimeWiring buildWiring() {
         return RuntimeWiring.newRuntimeWiring()
                 .type(newTypeWiring("Query")
-                        .dataFetcher("about", graphQLDataFetchers.aboutDataFetcher())
-                        .dataFetcher("character", graphQLDataFetchers.characterDataFetcher())
-                        .dataFetcher("human", graphQLDataFetchers.humanDataFetcher())
-                        .dataFetcher("droid", graphQLDataFetchers.droidDataFetcher())
-                        .dataFetcher("starship", graphQLDataFetchers.starshipDataFetcher())
-                        .dataFetcher("hero", graphQLDataFetchers.heroDataFetcher())
-                        .dataFetcher("search", graphQLDataFetchers.searchDataFetcher())
-                        .dataFetcher("reviews", graphQLDataFetchers.reviewsDataFetcher()))
+                        .dataFetcher("about", graphQLDataFetchers::getAboutMessage)
+                        .dataFetcher("character", graphQLDataFetchers::getcharacter)
+                        .dataFetcher("human", graphQLDataFetchers::getHuman)
+                        .dataFetcher("droid", graphQLDataFetchers::getDroid)
+                        .dataFetcher("starship", graphQLDataFetchers::getStarship)
+                        .dataFetcher("hero", graphQLDataFetchers::getHero)
+                        .dataFetcher("search", graphQLDataFetchers::search)
+                        .dataFetcher("reviews", graphQLDataFetchers::getReviews))
                 .type(newTypeWiring("Mutation")
-                        .dataFetcher("createReview", graphQLDataFetchers.createReviewDataFetcher())
-                        .dataFetcher("setAboutMessage", graphQLDataFetchers.setAboutMessage()))
+                        .dataFetcher("createReview", graphQLDataFetchers::createReview)
+                        .dataFetcher("setAboutMessage", graphQLDataFetchers::setAboutMessage))
                 .type(newTypeWiring("Character")
-                        .typeResolver(graphQLTypeResolver.characterTypeResolver))
+                        .typeResolver(graphQLTypeResolver::resolveObjectType))
                 .type(newTypeWiring("Human")
-                        .dataFetcher("friends", graphQLDataFetchers.friendsDataFetcher())
-                        .dataFetcher("starships", graphQLDataFetchers.humanStarshipsDataFetcher()))
+                        .dataFetcher("friends", graphQLDataFetchers::getFriends)
+                        .dataFetcher("starships", graphQLDataFetchers::getStarshipsByHuman))
                 .type(newTypeWiring("Droid")
-                        .dataFetcher("friends", graphQLDataFetchers.friendsDataFetcher()))
+                        .dataFetcher("friends", graphQLDataFetchers::getFriends))
                 .type(newTypeWiring("Starship")
-                        .dataFetcher("length", graphQLDataFetchers.starshipLengthDataFetcher()))
+                        .dataFetcher("length", graphQLDataFetchers::getStarshipLength))
                 .type(newTypeWiring("SearchResult")
-                        .typeResolver(graphQLTypeResolver.characterTypeResolver))
+                        .typeResolver(graphQLTypeResolver::resolveObjectType))
                 .build();
     }
 
