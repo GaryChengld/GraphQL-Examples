6f10add363d6f83cd0bc1828520fc057ea506728
Gary Cheng <chengld@yahoo.com>
2019-10-09 16:15:59 -0400

    Update MainVerticle.java

--- vertx/src/main/java/com/example/vertx/graphql/MainVerticle.java
+++ vertx/src/main/java/com/example/vertx/graphql/MainVerticle.java
@@ -1,6 +1,6 @@
 package com.example.vertx.graphql;
 
-import io.reactivex.Single;
+import io.reactivex.Completable;
 import io.vertx.core.Promise;
 import io.vertx.reactivex.core.AbstractVerticle;
 import io.vertx.reactivex.core.Vertx;
@@ -14,18 +14,19 @@ public class MainVerticle extends AbstractVerticle {
     @Override
     public void start(Promise<Void> startPromise) {
         this.startHttpServer(vertx)
-            .subscribe(server -> startPromise.complete(), startPromise::fail);
+                .subscribe(startPromise::complete, startPromise::fail);
     }
 
-    private Single<HttpServer> startHttpServer(Vertx vertx) {
+    private Completable startHttpServer(Vertx vertx) {
         logger.debug("Starting http server...");
         HttpServer server = vertx.createHttpServer()
-            .requestHandler(req -> {
-                req.response()
-                    .putHeader("content-type", "text/plain")
-                    .end("Hello from Vert.x!");
-            });
+                .requestHandler(req ->
+                        req.response()
+                                .putHeader("content-type", "text/plain")
+                                .end("Hello from Vert.x!")
+                );
         return server.rxListen(8888)
-            .doOnSuccess(httpServer -> logger.debug("Http server started on port 8888"));
+                .ignoreElement()
+                .doOnComplete(() -> logger.debug("Http server started on port 8888"));
     }
 }
