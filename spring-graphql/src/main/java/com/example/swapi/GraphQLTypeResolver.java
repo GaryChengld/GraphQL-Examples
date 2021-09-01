ca859457298563b64a8e00278eb38b7a612f2385
GaryChengld <chengld@yahoo.com>
2021-08-31 22:37:59 -0400

    Type Resolver

---
+++ spring-graphql/src/main/java/com/example/swapi/GraphQLTypeResolver.java
@@ -0,0 +1,27 @@
+package com.example.swapi;
+
+import com.example.swapi.entity.Droid;
+import com.example.swapi.entity.Human;
+import graphql.TypeResolutionEnvironment;
+import graphql.schema.GraphQLObjectType;
+import graphql.schema.TypeResolver;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Gary Cheng
+ */
+@Component
+public class GraphQLTypeResolver implements TypeResolver {
+
+    @Override
+    public GraphQLObjectType getType(TypeResolutionEnvironment environment) {
+        Object object = environment.getObject();
+        if (object instanceof Human) {
+            return (GraphQLObjectType) environment.getSchema().getType("Human");
+        } else if (object instanceof Droid) {
+            return (GraphQLObjectType) environment.getSchema().getType("Droid");
+        } else {
+            return (GraphQLObjectType) environment.getSchema().getType("Starship");
+        }
+    }
+}
--- spring-graphql/src/main/java/com/example/swapi/SwapiController.java
+++ spring-graphql/src/main/java/com/example/swapi/SwapiController.java
@@ -1,11 +1,15 @@
 package com.example.swapi;
 
+import com.example.swapi.entity.Droid;
 import com.example.swapi.entity.Human;
 import com.example.swapi.entity.MovieCharacter;
+import graphql.TypeResolutionEnvironment;
+import graphql.schema.GraphQLObjectType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.GraphQlController;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 
 /**
  * @author Gary Cheng
@@ -29,4 +33,18 @@ public class SwapiController {
     public Human human(@Argument String id) {
         return dataService.getHuman(id);
     }
+
+    @SchemaMapping
+    public GraphQLObjectType getType(TypeResolutionEnvironment environment) {
+        Object object = environment.getObject();
+        if (object instanceof Human) {
+            return (GraphQLObjectType) environment.getSchema().getType("Human");
+        } else if (object instanceof Droid) {
+            return (GraphQLObjectType) environment.getSchema().getType("Droid");
+        } else {
+            return (GraphQLObjectType) environment.getSchema().getType("Starship");
+        }
+    }
+
+
 }
