7e2f1cd586c60bbab1846db01a3b2c81ea5e81bb
GaryChengld <chengld@yahoo.com>
2021-09-12 22:33:36 -0400

    RuntimeWiringConfigurer

---
+++ spring-graphql/src/main/java/com/example/swapi/SwapiConfigurer.java
@@ -0,0 +1,28 @@
+package com.example.swapi;
+
+import graphql.schema.idl.RuntimeWiring;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.graphql.execution.RuntimeWiringConfigurer;
+
+import static graphql.schema.idl.TypeRuntimeWiring.newTypeWiring;
+
+/**
+ * @author Gary Cheng
+ */
+@Configuration
+@Slf4j
+public class SwapiConfigurer implements RuntimeWiringConfigurer {
+    private GraphQLTypeResolver graphQLTypeResolver;
+
+    public SwapiConfigurer(GraphQLTypeResolver graphQLTypeResolver) {
+        this.graphQLTypeResolver = graphQLTypeResolver;
+    }
+
+    @Override
+    public void configure(RuntimeWiring.Builder builder) {
+        log.debug("SwapiConfigurer");
+        builder.type(newTypeWiring("Character")
+                .typeResolver(graphQLTypeResolver));
+    }
+}
