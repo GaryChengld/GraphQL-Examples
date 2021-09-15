4cee714cd8be9e8e07b19bfd479dbfef45da336a
GaryChengld <chengld@yahoo.com>
2021-09-14 22:24:38 -0400

    length unit

--- spring-graphql/src/main/java/com/example/swapi/SwapiController.java
+++ spring-graphql/src/main/java/com/example/swapi/SwapiController.java
@@ -2,13 +2,17 @@ package com.example.swapi;
 
 import com.example.swapi.entity.Droid;
 import com.example.swapi.entity.Human;
+import com.example.swapi.entity.LengthUnit;
 import com.example.swapi.entity.MovieCharacter;
+import graphql.schema.DataFetchingEnvironment;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.GraphQlController;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 
 import java.util.List;
+
+import static com.example.swapi.entity.LengthUnit.FOOT;
 
 /**
  * @author Gary Cheng
@@ -42,7 +46,21 @@ public class SwapiController {
     }
 
     @SchemaMapping
+    public Double height(Human human, DataFetchingEnvironment environment) {
+        LengthUnit unit = LengthUnit.valueOf(environment.getArgument("unit"));
+        return this.getLength(human.getHeight(), unit);
+    }
+
+    @SchemaMapping
     public List<MovieCharacter> friends(Droid droid) {
         return this.dataService.getFriends(droid.getId());
+    }
+
+    private Double getLength(Double length, LengthUnit unit) {
+        if (unit == FOOT) {
+            return length * 3.28084;
+        } else {
+            return length;
+        }
     }
 }
